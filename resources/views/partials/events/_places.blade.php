49d2407d1d6a2f8c1ee4a903170530b9fb0f8739
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2016-12-19 13:32:53 +0800

    LOBBY - the yellow border in place is not correctly bordered.

--- resources/views/partials/events/_places.blade.php
+++ resources/views/partials/events/_places.blade.php
@@ -1,8 +1,12 @@
+<style type="text/css">
+	.slide-content-item { margin-right: 2em; }
+</style>
+
 <div class="row">
 	<div id="owl-content" class="owl-carousel">
 		@foreach ($places as $place)
 			<div class="slide-content-item">
-				<img src="images/{{ $place->ImageThumbNail }}" alt="slide1"/>
+				<img src="images/{{ $place->ImageThumbNail }}" alt="slide1" class="img-responsive" />
 				<div class="slide-title" value="{{ $place->IDPlace }}" ng-click="getPlaceID($event)">
 					<h2><span>{{ $place->PlaceName }}</span></h2>
 				</div>
