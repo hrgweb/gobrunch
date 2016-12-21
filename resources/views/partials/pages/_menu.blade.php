1b298f3ca370d34cfbfeeef159783b22be6fcdc8
Hergen Roque Gamayon <gamayonhergenroque.web@gmail.com>
2016-12-21 15:48:51 +0800

    Before Signing up, just show the about brunch and contact in the hamburger menu

--- resources/views/partials/pages/_menu.blade.php
+++ resources/views/partials/pages/_menu.blade.php
@@ -1,10 +1,14 @@
 <div class="overlay-content">
-	<a href="{{ url('lobby') }}">LOBBY</a>
-	<a href="{{ url('office') }}">MYOFFICE</a>
-	<a href="{{ url('account') }}">MY ACCOUNT</a>
-	<a href="{{ url('about') }}">ABOUT</a>
-	<a href="{{ url('contact') }}">CONTACT</a>
-	<a href="{{ action('UsersController@logout') }}">SIGN OUT</a>
+	@if (Auth::check())
+		<a href="{{ url('lobby') }}">LOBBY</a>
+		<a href="{{ url('office') }}">MYOFFICE</a>
+		<a href="{{ url('account') }}">MY ACCOUNT</a>
+		<a href="{{ action('UsersController@logout') }}">SIGN OUT</a>
+	@else
+		<a href="{{ url('about') }}">ABOUT</a>
+		<a href="{{ url('contact') }}">CONTACT</a>
+	@endif
+	
 	{{-- <a href="{{ action('UsersController@logout') }}" 
        onclick="event.preventDefault();
                 document.getElementById('form-logout').submit();">
